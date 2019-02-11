ac9ebbabce67849a55adf41c70c04474e30d170f
Alex Juhasz <gruunldfuulk@gmail.com>
2019-02-11 12:21:18 -0500

    V0.1

--- FinalCalulator/MainWindow.xaml.cs
+++ FinalCalulator/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace FinalCalulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string _units;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeWindowsElements();
         }
+
+        private void InitializeWindowsElements()
+        {
+            _units = "¢";
+
+            lbl_Pennies.Content = "Pennies (" + _units + ")";
+            lbl_Nickles.Content = "Nickles (" + _units + ")";
+            lbl_Dimes.Content = "Dimes (" + _units + ")";
+            lbl_Quarters.Content = "Quarters (" + _units + ")";
+            lbl_Total.Content = "Total Amount (" + _units + ")";
+
+        }
+
+        private void Button_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Button_Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double total;
+
+            total = double.Parse(txtbox_Pennies.Text) + double.Parse(txtbox_Nickles.Text) + double.Parse(txtbox_Dimes.Text) + double.Parse(txtbox_Quarters.Text);
+
+            txtbox_Total.Text = total;
+
+            SolutionWindow solutionWindow = new SolutionWindow(total);
+
+            solutionWindow.ShowDialog();
+        }
+
     }
+
 }
